302f3d3c30ae89bb2b001a940a46c5694e19ecd2
Jkutkut <45950243+Jkutkut@users.noreply.github.com>
2023-07-03 18:48:07 +0200

    able to place pixel on screen

--- src/main.c
+++ src/main.c
@@ -6,19 +6,46 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/03 17:59:17 by jre-gonz          #+#    #+#             */
-/*   Updated: 2023/07/03 18:10:09 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2023/07/03 18:47:53 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <mlx.h>
 
+typedef struct	s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
+
 int	main(void)
 {
 	void	*mlx;
 	void	*mlx_win;
+	t_data	img;
 
 	mlx = mlx_init();
 	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	img.img = mlx_new_image(mlx, 1920, 1080);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+								&img.endian);
+	int color = 0x01000000;
+	for (int i = 0, j; i < 1920; i++) {
+		for (j = 0; j < 1080; j++) {
+			my_mlx_pixel_put(&img, i, j, color);
+			color = color + 0x00001;
+		}
+	}
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
 	mlx_loop(mlx);
-	return (0);
 }
